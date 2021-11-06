9caaddd547edd44ed83f89f71c8c2c9a20b0dfb1
ahmed_ishraq <ishraqahmedbd@gmail.com>
2021-11-07 01:57:29 +0600

    Lab 2 update

--- Lab Works/Lab_2.sql
+++ Lab Works/Lab_2.sql
@@ -1,2 +1,5 @@
 01. Find all the unique multipliers in the table and show them in descending order.
 => SELECT DISTINCT multiplier FROM users ORDER BY multiplier DESC;
+
+02. Show all the unique dates of the users joining in ascending order.
+=> SELECT DISTINCT member_since FROM users ORDER BY influence_count;
