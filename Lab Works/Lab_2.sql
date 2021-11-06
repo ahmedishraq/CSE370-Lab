9bc7bddead325b5e28b437ce6e76f7aa75f997e5
ahmed_ishraq <ishraqahmedbd@gmail.com>
2021-11-07 02:06:43 +0600

    update lab 2

--- Lab Works/Lab_2.sql
+++ Lab Works/Lab_2.sql
@@ -3,3 +3,6 @@
 
 02. Show all the unique dates of the users joining in ascending order.
 => SELECT DISTINCT member_since FROM users ORDER BY influence_count;
+
+03. Find all ids and email addresses where the TLD (Top Level Domain) is ‘com’ (ends with ‘com’)
+=> SELECT id, email FROM users WHERE email LIKE '%com'
