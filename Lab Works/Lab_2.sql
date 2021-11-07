cd13c64a511e3a1943f81e66c2ecbd485106e2db
ahmed_ishraq <ishraqahmedbd@gmail.com>
2021-11-07 21:37:16 +0600

    update lab 2

--- Lab Works/Lab_2.sql
+++ Lab Works/Lab_2.sql
@@ -5,7 +5,7 @@
 => SELECT DISTINCT member_since FROM users ORDER BY influence_count;
 
 03. Find all ids and email addresses where the TLD (Top Level Domain) is ‘com’ (ends with ‘com’)
-=> SELECT id, email FROM users WHERE email LIKE '%com'
+=> SELECT id, email FROM users WHERE email LIKE '%com';
 
 04. Show the name and email of the latest 5 members
 => SELECT name, email FROM users ORDER BY member_since DESC LIMIT 5;
