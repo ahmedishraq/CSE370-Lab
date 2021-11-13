678abac71f122c4cbf6ad03695b38927c60abc8a
ahmed_ishraq <ishraqahmedbd@gmail.com>
2021-11-14 01:08:34 +0600

    update lab 3

--- Lab Works/Lab_3.sql
+++ Lab Works/Lab_3.sql
@@ -1,2 +1,19 @@
+-> just making the table
+=> create table employess(
+	employee_id char(10),
+	first_name varchar(20),
+	last_name varchar(20),
+	email varchar(60),
+	phone_number char(14),
+	hire_date date,
+	job_id int,
+	salary int,
+	commission_pct decimal(5,3),
+	manager_id char(10),
+	department_id int
+);
+
+
+
 1. Find the first_name, last_name, email, phone_number, hire_date and department_id of all the employees with the latest hire_date in each department. 
 => 
