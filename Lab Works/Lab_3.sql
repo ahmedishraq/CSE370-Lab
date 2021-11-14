db68aa3c4db8a782a51e823c5bb0dc1632b75575
ahmed_ishraq <ishraqahmedbd@gmail.com>
2021-11-14 19:48:43 +0600

    update lab 3

--- Lab Works/Lab_3.sql
+++ Lab Works/Lab_3.sql
@@ -15,5 +15,14 @@
 
 
 
-1. Find the first_name, last_name, email, phone_number, hire_date and department_id of all the employees with the latest hire_date in each department. 
-=> SELECT first_name, last_name, email, phone_number, hire_date, department_id FROM employess ORDER BY hire_date; 
+1. *Find the first_name, last_name, email, phone_number, hire_date and department_id of all the employees with the latest hire_date in each department. 
+=> SELECT first_name, last_name, email, phone_number, hire_date, department_id FROM employess
+   ORDER BY hire_date DESC;
+
+2. *Find the first_name, last_name, email, phone_number, hire_date and department_id of all the employees with the oldest  hire_date in each department. 
+=> SELECT first_name, last_name, email, phone_number, hire_date, department_id FROM employess
+   ORDER BY hire_date;
+
+3. Find the first_name, last_name, employee_id, phone_number, salary and department_id of all the employees with the lowest salary in each department. 
+=> SELECT first_name, last_name, employee_id, phone_number, salary, department_id FROM employess
+   WHERE (department_id, salary) IN (SELECT department_id, MIN(salary) FROM employess GROUP BY department_id);  
