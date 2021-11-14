84562ad9dba6d6d0a68486758f46cb407f69bf11
ahmed_ishraq <ishraqahmedbd@gmail.com>
2021-11-14 19:05:58 +0600

    update lab 3

--- Lab Works/Lab_3.sql
+++ Lab Works/Lab_3.sql
@@ -16,4 +16,4 @@
 
 
 1. Find the first_name, last_name, email, phone_number, hire_date and department_id of all the employees with the latest hire_date in each department. 
-=> 
+=> SELECT first_name, last_name, email, phone_number, hire_date, department_id FROM employess ORDER BY hire_date; 
